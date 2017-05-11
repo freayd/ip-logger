86ffe267ff2b0fa3521785388dc785c9bad1a420
freayd <freayd@users.noreply.github.com>
2017-05-11 10:55:05 +0200

    Remove Nokogiri dependency

--- ip_logger.rb
+++ ip_logger.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'bundler/setup'
-require 'nokogiri'
 require 'ipaddr'
 require 'json'
 require 'open-uri'
@@ -15,9 +12,9 @@ now = Time.now
 # - http://dyn.com/support/developers/checkip-tool/
 # - https://support.google.com/fiber/answer/2899098?hl=en
 # - http://forum.dyndnscommunity.com/forum/viewtopic.php?f=17&t=23
-dyndns_s = Nokogiri::HTML(open('http://checkip.dyndns.org/')).at_css('body').content[ip_regex] rescue nil
-dnsexit_s = Nokogiri::HTML(open('http://ip.dnsexit.com/')).at_css('body').content[ip_regex] rescue nil
-duckduckgo_s = JSON.parse(Nokogiri::HTML(open('https://api.duckduckgo.com/?q=ip&format=json')).at_css('body').content)['Answer'][ip_regex] rescue nil
+dyndns_s = open('http://checkip.dyndns.org/').read[ip_regex] rescue nil
+dnsexit_s = open('http://ip.dnsexit.com/').read[ip_regex] rescue nil
+duckduckgo_s = JSON.parse(open('https://api.duckduckgo.com/?q=ip&format=json').read)['Answer'][ip_regex] rescue nil
 dyndns_ip     = IPAddr.new(dyndns_s    ).to_s if dyndns_s
 dnsexit_ip    = IPAddr.new(dnsexit_s   ).to_s if dnsexit_s
 duckduckgo_ip = IPAddr.new(duckduckgo_s).to_s if duckduckgo_s
