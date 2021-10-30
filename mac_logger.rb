6b4a18fbcca5c35d996da3fdbeebc62d3411a303
freayd <freayd@users.noreply.github.com>
2021-10-30 09:48:48 +0200

    Log the Wi-Fi MAC address

---
+++ mac_logger.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+log = File.join(File.dirname(__FILE__), 'macs.log')
+now = Time.now
+
+mac = `spoof-mac list --wifi`.chomp
+
+# Exit if no MAC address found
+exit if mac.empty?
+
+# Read previous message from log
+previous_message = nil
+if File.readable?(log)
+    begin
+        last_line = File.readlines(log).last
+        previous_message = last_line[/\] - (.*)\s*$/, 1] if last_line
+    rescue ArgumentError
+    end
+end
+
+# Log if the message changed
+message = "#{mac}"
+File.open(log, 'a') {|f| f.puts("[#{now}] - #{message}") } if message != previous_message
